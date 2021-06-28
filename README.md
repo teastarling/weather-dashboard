7ef957cbaaab38d9b7421ef12c9b28de743a5bd4
Tyler Starling <tyler.a.starling@gmail.com>
2021-06-28 19:06:25 -0400

    big changes made to code. trimmed down html test view content in order to create it dynamically in javascript. connected existing elements with data called from api calls in order to fill out or generate empty fields accordingly. created button element from local storage and switched local storage to an array instead of keys corresponding to value. made generated storage buttons clickable

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# weather-dashboard
+# Weather Dashboard
+
+A weather application utilizing api calls to gather data displayed within dynamically generated elements.
+
+## Appearance
+
+Appearance seen [here](./assets/img/weatherss.png)
+
+## Deployed Application
+
+Application can be viewed [here](https://teastarling.github.io/weather-dashboard/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
+// Variables needed to access different html elements, along with the parse to retrieve the array from local storage or to pull an empty array if nothing available
 var cityInputEl = document.getElementById("city-input");
-var searchButtonEl = document.getElementById("search-btn")
-var urlNewYork = "https://api.openweathermap.org/data/2.5/forecast?q=New York&appid=427c72356bf2f2c7899a5613b020d43f";
-var cityArray = [];
+var searchButtonEl = document.getElementById("search-btn");
+var cardBlockEl = document.getElementById("card-blocks");
+var todayListEl = document.getElementById("one-day-forecast");
+var buttonList = document.getElementById("storage-buttons");
+var cityArray = JSON.parse(localStorage.getItem("cities")) || [];
 
-function fiveDayUrl(event) {
+// function that runs on "search" that creates the variable run through weatherFetch so that it can be assigned to input field or to button value
+function onClick(event) {
     event.preventDefault();
+    var cityInputVal = cityInputEl.value;
+    weatherFetch(cityInputVal);
+}
 
-    var cityInputVal = cityInputEl.value
-    
-    localStorage.setItem(cityInputVal, cityInputVal);
+// function to run the weatherFetch function with the button value if button is clicked instead of search input
+function buttonCall(event) {
+    event.preventDefault();
+    weatherFetch(this.value);
+}
+
+// first adds the input to the cityArray in local storage, then runs the fetch call with the cityInputVal (city name from input field or button) taking the place of the city name in the URL. From there takes in the latitude and longitude for that city, as well as the city name since this is not included in the next batch of info, and then runs another fetch using that lat and lon in order to fetch the onecall api which includes the UVI
+function weatherFetch(cityInputVal){
+
+    cityArray.push(cityInputVal);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+    createButton(cityInputVal);
     
     var citySearchUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInputVal + "&appid=427c72356bf2f2c7899a5613b020d43f";
 
@@ -19,6 +35,9 @@ function fiveDayUrl(event) {
     .then(function(data) {
         var lat = data.city.coord.lat;
         var lon = data.city.coord.lon;
+        var cityEl = document.getElementById("city-header");
+
+        cityEl.textContent = data.city.name;
 
         var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=427c72356bf2f2c7899a5613b020d43f"
 
@@ -27,11 +46,107 @@ function fiveDayUrl(event) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data)
+            createTodayForecast(data);
         });
     })
-
-
 }
 
-searchButtonEl.addEventListener('click', fiveDayUrl);
+// Today's forecast in the jumbotron is filled out with data pulled from the api call, with a list being created for some values, and the UVI is colored according to severity. then a loop is run to create the 5-day cards
+function createTodayForecast(data) {
+    var headerDateEl = document.getElementById("date-header");
+    var headerIconEl = document.getElementById("icon-header");
+    var dataDate = data.daily[0].dt;
+    var todayIcon = data.daily[0].weather[0].icon;
+    var headerImg = "https://openweathermap.org/img/wn/" + todayIcon + ".png";
+    
+    todayListEl.innerHTML = "";
+
+    headerDateEl.textContent = moment.unix(dataDate).format("l");
+    headerIconEl.setAttribute("src", headerImg);
+
+    var todayTemp = document.createElement('li');
+    todayTemp.textContent =  "Temp: " + data.daily[0].temp.max + "°";
+
+    var todayWind = document.createElement('li');
+    todayWind.textContent = "Wind: " + data.daily[0].wind_speed + "mph";
+
+    var todayHumidity = document.createElement('li');
+    todayHumidity.textContent = "Humidity: " + data.daily[0].humidity + "%";
+
+    var todayUvi = document.createElement('li');
+    todayUvi.textContent = "UVI: " + data.current.uvi;
+    if (data.current.uvi <= 4) {
+        todayUvi.setAttribute("class", "uvi-low")
+    } else if (data.current.uvi <= 8) {
+        todayUvi.setAttribute("class", "uvi-mid")
+    } else {
+        todayUvi.setAttribute("class", "uvi-high")
+    }
+    
+    todayListEl.append(todayTemp, todayWind, todayHumidity, todayUvi)
+
+    cardBlockEl.innerHTML = "";
+
+    for(var i=1; i < 6; i++) {
+        fiveDay(data.daily[i]);
+    }
+}
+
+// loop runs through this function to create all the 5 day cards with data from the api call using the correct styling classes 
+function fiveDay(daily) {
+    var dateUnix = daily.dt
+    var dailyIcon = daily.weather[0].icon;
+    var dailyImg = "https://openweathermap.org/img/wn/" + dailyIcon + ".png";
+
+    var resultCol = document.createElement('div');
+    resultCol.classList.add('col-2');
+
+    var resultCard = document.createElement('div');
+    resultCard.classList.add('card');
+    resultCol.append(resultCard)
+
+    var resultCardLook = document.createElement('div');
+    resultCardLook.classList.add("card-body", "bg-dark", "text-white")
+    resultCard.append(resultCardLook);
+
+    var resultHead = document.createElement('h6');
+    resultHead.classList.add('card-title')
+    resultHead.textContent = moment.unix(dateUnix).format("l");
+    resultCardLook.append(resultHead);
+    
+    var resultUl = document.createElement('ul');
+    resultUl.classList.add('card-text');
+    resultHead.append(resultUl);
+
+    var resultIcon = document.createElement("img");
+    resultCol.setAttribute("src", dailyImg);
+
+    var tempLi = document.createElement('li');
+    tempLi.textContent = "Temp: " + daily.temp.max + "°";
+
+    var windLi = document.createElement('li');
+    windLi.textContent = "Wind: " + daily.wind_speed + "mph";
+
+    var humidLi = document.createElement('li');
+    humidLi.textContent = "Humidity: " + daily.humidity + "%";
+
+    resultUl.append(resultIcon, tempLi, windLi, humidLi)
+
+    cardBlockEl.append(resultCol);
+}
+
+// on page load, for-loop is run that triggers the "create button" function each time it runs through it in order to generate buttons on the side with previously entered cities stored in the local storage array
+for (var i=0; i < cityArray.length; i++) {
+    createButton(cityArray[i]);
+}
+function createButton(cityName) {
+    var button = document.createElement('button');
+    button.classList.add("storagebtn", "btn", "btn-secondary", "col-md-12", "m-2");
+    button.setAttribute("value", cityName);
+    button.textContent = cityName;
+    buttonList.append(button);
+}
+
+// click events corresponding to search button and local storage buttons 
+searchButtonEl.addEventListener('click', onClick);
+document.querySelector(".storagebtn").addEventListener('click', buttonCall);
